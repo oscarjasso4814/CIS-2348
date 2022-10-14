ff8fd95c54680a6c3eadebdef9f5bfdd6db6c3e4
oscarjasso4814 <112199841+oscarjasso4814@users.noreply.github.com>
2022-10-14 02:13:48 -0500

    part c. output is written on new file

--- Homework2/coding_problem_2.py
+++ Homework2/coding_problem_2.py
@@ -26,7 +26,6 @@ today_month = today.month
 today_day = today.day
 today_year = today.year
 
-
 # open and read through the file
 # create list from the data
 my_file = open("inputData.txt", "r")
@@ -34,24 +33,25 @@ data = my_file.read()
 dates = data.split("\n")
 
 # iterate through dates in list, if date meets format requirements (have a ',') then append it to a new list.
-# use find().
+# use find()
 new_dates = []
-for i in dates:
-    find_comma = i.find(',')
-    if find_comma != -1:
-        new_dates.append(i)
+for n in dates:
+    form_date = n.find(',')
+    if form_date != -1:
+        new_dates.append(n)
 
-# iterate through dates in new_dates list and split info into parts.
+# open parsedDates.txt
+# iterate through dates in correct format list and split info into parts.
 # ignores any month that isn't a key from the dictionary(wrong format),cant be called.
-# delete the ',' in month by replacing it with a blank space.
-# make sure date is before the current date.
-# reformat and print date.
+# delete the ',' by replacing it with a blank space.
+# make sure date is before the current date
+# reformat and write into parsedDates.txt
+f = open('parsedDates.txt', 'w')
 for x in new_dates:
     split_date = x.split()
     month = months[split_date[0]]
     day = split_date[1].replace(',', '')
     year = split_date[2]
-
     if int(year) > today_year:
         break
     elif int(year) == today_year and int(month) > today_month:
@@ -60,4 +60,5 @@ for x in new_dates:
         break
     else:
         date = f'{month}/{day}/{year}'
-        print(date)
+        f.write(date)
+        f.write('\n')
