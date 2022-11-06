bc6861f4b280e0d02abf1e0cd4f030216faa6de4
oscarjasso4814 <112199841+oscarjasso4814@users.noreply.github.com>
2022-11-05 21:50:11 -0500

    Add files via upload

---
+++ Homework3/Zylabs_11_22.py
@@ -0,0 +1,17 @@
+# Oscar Jasso
+# PSID : 1895743
+
+
+words = input()
+word_list = words.split()
+
+for word in word_list:
+    count = word_list.count(word)
+    print(word, count)
+    
+    
+
+
+
+    
+
